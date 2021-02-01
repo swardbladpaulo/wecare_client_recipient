a42ef7e546ee6c3e36e2b3712a5e0c5f38112481
flis_ann <ann.flismark@gmail.com>
2021-02-01 10:19:38 +0100

    Cleaning up styling

--- components/StartScreen.jsx
+++ components/StartScreen.jsx
@@ -16,15 +16,11 @@ import donor3 from '../assets/images/donor3.png'
 const StartScreen = () => {
   return (
     <>
-      <View >
+      <View>
         <Image style={styles.image} source={wecare_logo} />
         {/* <Text style={styles.topText}>Welcome to weCare </Text> */}
         <Text style={styles.bodyText}>
           Here you can register for an account and find a foodbag near you!
-        </Text>
-        <Text style={styles.bodyText}>
-          We have signed some of the most popular food stores in Sweden that
-          will ensure the highest food quality
         </Text>
         <Text style={styles.partner}>Our partners</Text>
         <View style={styles.foodStore}>
@@ -32,6 +28,10 @@ const StartScreen = () => {
           <Image style={styles.storeImage} source={donor2} />
           <Image style={styles.storeImage} source={donor3} />
         </View>
+        <Text style={styles.bodyText}>
+          We have signed some of the most popular food stores in Sweden that
+          will ensure the highest food quality
+        </Text>
         <View style={styles.parent}>
           <TouchableOpacity activeOpacity={0.75} style={styles.button}>
             <Text style={styles.text}>Register Here</Text>
@@ -45,37 +45,20 @@ const StartScreen = () => {
 export default StartScreen
 
 const styles = StyleSheet.create({
-  topText: {
-    backgroundColor: '#993576',
-    fontSize: 35,
-    color: 'white',
-    textAlign: 'center',
-    padding: 10,
-    fontWeight: 'bold',
-    paddingBottom: 10,
-  },
-  headerText: {
-    backgroundColor: 'white',
-    fontSize: 28,
-    color: 'black',
-    textAlign: 'center',
-  },
   bodyText: {
     backgroundColor: 'white',
-    fontSize: 20,
+    fontSize: 25,
     color: '#4a4443',
-    textAlign: 'left',
-    padding: 15,
-    paddingBottom: 20,
+    textAlign: 'justify',
+    padding: 25,
   },
   image: {
     height: 120,
-    width: Dimensions.get('window').width,
-    paddingBottom: 6,
   },
   storeImage: {
     height: 90,
     width: 90,
+    borderRadius: 20,
   },
   foodStore: {
     flexDirection: 'row',
@@ -84,30 +67,31 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 10,
   },
-    partner: {
+  partner: {
     fontSize: 25,
-    backgroundColor: '#a34983',
+    backgroundColor: '#d1619f',
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
     paddingBottom: 10,
     paddingTop: 10,
   },
- parent: {
-   backgroundColor: 'white',
- },
+  parent: {
+    backgroundColor: 'white',
+    paddingBottom: 30,
+  },
 
   button: {
     flexDirection: 'row',
     height: 65,
-    backgroundColor: '#993576',
+    backgroundColor: '#d1619f',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 50,
+    marginTop: 40,
     elevation: 3,
     margin: 39,
-    borderRadius: 20
-    
+    borderRadius: 20,
+  
   },
   text: {
     fontSize: 25,
