cc55ef094b2c25cf97f19dcb69c8b62fd61413c3
emtalen <59577366+emtalen@users.noreply.github.com>
2021-02-05 12:11:31 +0100

    fixing a swipe down update to update the feed

--- components/DisplayFoodBagsList.jsx
+++ components/DisplayFoodBagsList.jsx
@@ -1,41 +1,54 @@
-import React, { useEffect } from 'react'
-import { View, FlatList, ScrollView } from 'react-native'
-import { useSelector } from 'react-redux'
-import { StatusBar } from 'expo-status-bar'
-import DisplayFoodBag from './DisplayFoodBag'
-import FoodBagService from '../modules/FoodBagService'
+import React, { useEffect, useState, useCallback } from "react";
+import { View, FlatList, ScrollView, RefreshControl } from "react-native";
+import { useSelector } from "react-redux";
+import { StatusBar } from "expo-status-bar";
+import DisplayFoodBag from "./DisplayFoodBag";
+import FoodBagService from "../modules/FoodBagService";
 
 const DisplayFoodBagsList = ({ navigation }) => {
-  const { foodbags } = useSelector((state) => state)
+  const { foodbags } = useSelector((state) => state);
+  const [refresh, setRefresh] = useState(false);
   useEffect(() => {
-    FoodBagService.index()
-  }, [])
-  return (
-    
-    <ScrollView style={{ flex: 1}}>
-    <View style={styles.container}>
-      <StatusBar style='auto' />
-      <FlatList
-        data={foodbags}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <DisplayFoodBag navigation={navigation} foodbag={item} />
-        )}
-      />
-    </View>
-    </ScrollView>
-  )
-}
+    FoodBagService.index();
+  }, []);
 
-export default DisplayFoodBagsList
+  const onRefresh = useCallback( async () => {
+    setRefresh(true);
+    await FoodBagService.index().then(() => {
+      setRefresh(false);
+    });
+  }, []);
+
+  return (
+    <ScrollView
+      style={{ flex: 1 }}
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }
+    >
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <FlatList
+          data={foodbags}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <DisplayFoodBag navigation={navigation} foodbag={item} />
+          )}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default DisplayFoodBagsList;
 
 const styles = {
   headerStyle: {
-    backgroundColor: '#ce93b6',
+    backgroundColor: "#ce93b6",
   },
   headerTitleStyle: {
-    color: '#ffe8d4',
+    color: "#ffe8d4",
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-}
+};
